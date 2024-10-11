0e687862f4b1bd37407963bd12bb0d2b01a090b2
Haythem <heythem.94@gmail.com>
2024-10-11 13:36:42 +0100

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let numberOfArguments = process.argv.length ;
+
+if ( numberOfArguments > 2)
+
+        {
+        console.log('Arguments found');
+
+        } else {
+                console.log('No Arguments');
+                }
