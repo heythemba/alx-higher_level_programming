564bb6155542138ec88d204a660a2d171ab107be
Haythem <heythem.94@gmail.com>
2024-10-12 12:29:55 +0100

    JavaScript Warm-up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,12 +3,12 @@
 // A script that prints "x" times "C is fun"
 
 const numberOccurance = process.argv[2];
-let counter=0;
+let counter = 0;
 
 if (!isNaN(numberOccurance)) {
-for (counter= 0;counter <= numberOccurance; counter ++){
-        console.log('C is fun');
-}
+  for (counter = 0; counter <= numberOccurance; counter++) {
+    console.log('C is fun');
+  }
 } else {
-console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
