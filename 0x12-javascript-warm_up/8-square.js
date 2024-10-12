e9a3b51154c46f40d5c5932d199fd852dcaa4e1f
Haythem <heythem.94@gmail.com>
2024-10-12 13:16:10 +0100

    JavaScript Warm-up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// A script that print squares with X
+
+const squareDim = process.argv[2];
+let counter1 = 0;
+let counter2 = 0;
+
+if (!isNaN(squareDim)) {
+  for (counter1 = 0; counter1 < squareDim; counter1++) {
+    for (counter2 = 0; counter2 < squareDim; counter2++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
