53171b39e740691091a6c9fb1c2b785bbc5a29cb
Haythem <heythem.94@gmail.com>
2024-10-13 16:24:18 +0100

    JavaScript Warm-up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,26 @@
 #!/usr/bin/node
 
-// A script that search for the second biggest number
+// A script that searches for the second biggest number
 
 let firstbiggest = -Infinity;
 let secondbiggest = -Infinity;
 
-for (let count = 2; count <= (process.argv.length); count++) {
-  if (process.argv.length === 2 || process.argv.length === 3) {
-    secondbiggest = 0;
-  } else if (firstbiggest < Number(process.argv[count])) {
-    secondbiggest = firstbiggest;
-    firstbiggest = Number(process.argv[count]);
-  }
-}
+// If fewer than two numbers are provided, set secondbiggest to 0
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  // Loop through the arguments starting from the third element (index 2)
+  for (let count = 2; count < process.argv.length; count++) {
+    const num = Number(process.argv[count]);
 
-console.log(secondbiggest);
+    if (num > firstbiggest) {
+      secondbiggest = firstbiggest;
+      firstbiggest = num;
+    } else if (num > secondbiggest && num < firstbiggest) {
+      secondbiggest = num;
+    }
+  }
+
+  // After the loop, print the second biggest number
+  console.log(secondbiggest);
+}
