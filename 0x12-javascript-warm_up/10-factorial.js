d2afe1d3862c2e6f71b4734285df5fe89b1c0102
Haythem <heythem.94@gmail.com>
2024-10-13 15:14:19 +0100

    JavaScript Warm-up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/user/bin/node
+
+// A script that calculae factoriel
+
+const fact = process.argv[2];
+
+function factoriel (n) {
+  let f = 1;
+  if ((n === 0) || isNaN(n)) {
+    return f;
+  } else {
+    f = factoriel(n - 1) * n;
+    return f;
+  }
+}
+
+console.log(factoriel(fact));
