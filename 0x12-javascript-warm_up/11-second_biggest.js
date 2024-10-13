afa659004a28ac5c601dbb261fcf858a92cc8b29
Haythem <heythem.94@gmail.com>
2024-10-13 16:16:41 +0100

    JavaScript Warm-up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,13 @@
 
 // A script that search for the second biggest number
 
-let firstbiggest = 0;
-let secondbiggest = 0;
-let count;
-for (count = 0; count <= (process.argv.length); count++) {
-  if (firstbiggest < Number(process.argv[count])) {
+let firstbiggest = -Infinity;
+let secondbiggest = -Infinity;
+
+for (let count = 2; count <= (process.argv.length); count++) {
+  if (process.argv.length === 2 || process.argv.length === 3) {
+    secondbiggest = 0;
+  } else if (firstbiggest < Number(process.argv[count])) {
     secondbiggest = firstbiggest;
     firstbiggest = Number(process.argv[count]);
   }
