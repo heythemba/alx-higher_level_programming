2888147d3d1b3dba85682c2df23448822b88dce3
Haythem <heythem.94@gmail.com>
2024-10-14 11:04:52 +0100

    JavaScript Warm-up

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,10 +2,6 @@
 
 // script that print a funct x times
 
-function callMeMoby (x, a) {
-  for (let count = 0; count < x; count++) {
-    console.log('C is fun');
-  }
-}
-
-module.exports = { callMeMoby };
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
+};
